e1f81d829aa2c8331f477d689e0cb5e4f619f2c9
yangmin <157715848@qq.com>
2017-11-15 13:48:45 +0800

    commit meet

---
+++ com/facebook/react/react-native/0.49.5/react-native bundle --entry-file index.android.js
@@ -0,0 +1,1 @@
+react-native bundle --entry-file index.android.js --bundle-output ./android/app/sdk/main/assets/index.android.bundle --platform android --assets-dest ./android/sdk/src/main/res/ --dev false
